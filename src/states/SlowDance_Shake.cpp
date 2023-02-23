8c979792f3255fc1627730a459a72ccf7ed02404
Sorina Silvia Circu <sorina-silvia.circu@lirmm.fr>
2023-02-23 19:35:00 +0100

    retour max

--- src/states/SlowDance_Shake.cpp
+++ src/states/SlowDance_Shake.cpp
@@ -7,7 +7,6 @@ void SlowDance_Shake::start(mc_control::fsm::Controller & ctl_)
 
 {
   
-  
      
   jointIndex = ctl_.robot().jointIndexByName(jointName);
    
@@ -15,10 +14,15 @@ void SlowDance_Shake::start(mc_control::fsm::Controller & ctl_)
 
 
   postureTask = ctl_.getPostureTask(ctl_.robot().name()); 
+ 
   postureTask->stiffness(200);
   
   neckTask = std::make_shared<mc_tasks::PostureTask>(ctl_.solver(), ctl_.robot().robotIndex(), 100, 10);
-  neckTask->selectActiveJoints(ctl_.solver(), {"NECK_Y"}); 
+  
+  //value[0].push_back({{0.0}, {0.33}, {-0.33}, {0.66}, {-0.66}, {0.0}, {0.33}, {-0.33}, {0.66}, {-0.66}});
+
+  //neckTask->selectActiveJoints(ctl_.solver(), {"NECK_Y"}); 
+  value = 0.0;
 
 }
 
@@ -26,18 +30,49 @@ void SlowDance_Shake::start(mc_control::fsm::Controller & ctl_)
 bool SlowDance_Shake::run(mc_control::fsm::Controller & ctl_)
 {
 
- 
-  auto neck_posture = ctl_.robot().mbc().q;
- 
-  double value = 0.0;
+  //auto neck_posture = ctl_.robot().mbc().q; //prend les valeurs actuelles des joints 
+  
+  std::vector<double> neck_posture = ctl_.robot().q()[jointIndex]; // val du NECK_Y ?
+  mc_rtc::log::info("size = {}", neck_posture.size()); // size 1 et valeur 0.0
 
-  for (int a= 0; a< neck_posture.size(); a++){
-  neck_posture[jointIndex][0] = sin(2*M_PI*value);
-  value += 0.33;
-  mc_rtc::log::info("value = {}", neck_posture[jointIndex][0]);
-  }
+  neck_posture[0]+= 0.30;
+ // neck_posture[0] = {0.80};
+  mc_rtc::log::info("index val. = {}", neck_posture[0]);
+  //neckTask->target({{ jointName, neck_posture}});
+    //neckTask->posture(neck_posture[0]);
+   ctl_.robot().q()[jointIndex] = neck_posture;
 
-  neckTask->posture(neck_posture); 
+//   for (int i = 0; i < 10; i++)
+// {
+//         neck_posture[0]+= 0.30;
+//         
+//         mc_rtc::log::info("index val. = {}", neck_posture[0]);  
+// }
+
+  
+  
+  // for (int a= 0; a< neck_posture.size(); a++){
+  // neck_posture[jointIndex][0] = value;
+  // std::for_each(value.begin(), value.end(), [](double& d) { d+=0.33;});
+  
+  // mc_rtc::log::info("value = {}", neck_posture[jointIndex][0]);
+ 
+  // }
+
+
+// for (int a= 0; a< 10; a++){ //10 was before neck_posture.size()
+  
+//  value+= 0.33;
+  
+// // ref postureTask->target({{ jointName, robot().qu()[jointIndex] }});
+  
+// mc_rtc::log::info("value = {}", value);
+// //neckTask->target({{ jointName, {0.1*sin(value)} }});
+// } 
+
+
+//std::for_each(value[0].begin(), value[0].end(), [](double& d) { d+=0.33;});
+
 
   
   
--- src/states/SlowDance_Shake.h
+++ src/states/SlowDance_Shake.h
@@ -11,11 +11,18 @@ struct SlowDance_Shake : mc_control::fsm::State
     bool run(mc_control::fsm::Controller & ctl) override;
 
     void teardown(mc_control::fsm::Controller & ctl) override;
+
+    
 private:
 std::shared_ptr<mc_tasks::PostureTask> postureTask;
 std::shared_ptr<mc_tasks::PostureTask> neckTask;
 std::string jointName = "NECK_Y";
 int jointIndex = 0;
-
+std::vector<double> neck_posture;
+//std::vector<std::vector<std::vector<double>>> value;
+double value;
+unsigned iter_ = 0;
+int index_ = 0;
+int t_ = 200;
 
 };
